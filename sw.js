f2178c85437aa9c8ae11be28883331d004effa16
ttttooonni <antonio_guardia@digitechfp.com>
2026-09-18 17:36:02 +0100

    Update PWA asset cache paths

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE='hair-factory-v5';
 const ASSETS=[
  './','./index.html','./styles.css','./app.js','./manifest.webmanifest','./404.html','./robots.txt',
- './assets/logo.png','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','./icons/favicon-32.png',
- './assets/styles/png/taper-fade.png','./assets/styles/png/textured-crop.png','./assets/styles/png/buzz-cut.png','./assets/styles/png/low-fade.png',
- './assets/styles/png/clasico.png','./assets/styles/png/side-part.png','./assets/styles/png/crew-cut.png','./assets/styles/png/tijera.png',
- './assets/styles/png/barba-3.png','./assets/styles/png/barba-corta.png','./assets/styles/png/barba-media.png','./assets/styles/png/barba-larga.png'
+ './assets/logo.svg','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','./icons/favicon-32.png',
+ './assets/styles/taper-fade.png','./assets/styles/textured-crop.png','./assets/styles/buzz-cut.png','./assets/styles/low-fade.png',
+ './assets/styles/clasico.png','./assets/styles/side-part.png','./assets/styles/crew-cut.png','./assets/styles/tijera.png',
+ './assets/styles/barba-3.png','./assets/styles/barba-corta.png','./assets/styles/barba-media.png','./assets/styles/barba-larga.png'
 ];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
